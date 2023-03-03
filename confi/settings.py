ae94901e84f7841e558c41f9bf4ea2400f5e1926
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2023-03-03 23:46:35 +0500

    Update settings.py

--- confi/settings.py
+++ confi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+e$g@=9&$g$rhf9=&vgzu#2&ux1-$g(qsnxb0o0jx*i00(4f$&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.pythonanywhere.com']
 
