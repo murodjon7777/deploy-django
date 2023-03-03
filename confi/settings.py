c4533b9179972123e4c89ff377eb94758c590fb9
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2023-03-03 21:03:38 +0500

    static

--- confi/settings.py
+++ confi/settings.py
@@ -134,11 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 import os
-STATICFILES_DIRS=[
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS=[
+#     BASE_DIR / 'static'
+# ]
 STATIC_URL = 'static/'
 MEDIA_URL='/media/'
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
